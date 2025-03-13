cb4a842a64000d9d151506cbd4e892596ba2fa23
Audrey Lu <luay5@uci.edu>
2025-03-13 03:51:06 +0000

    Print path and total cost

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,1 +1,8 @@
 #include <dijkstras.h>
+
+void print_path(const vector<int>& v, int total) {
+    for (int vertex : v)
+        cout << vertex << " ";
+    cout << endl;
+    cout << "Total cost is " << total << endl;
+}
