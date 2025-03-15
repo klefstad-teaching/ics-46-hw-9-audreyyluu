04db542f65a199210fc0c67e261284acd5400de3
Audrey Lu <luay5@uci.edu>
2025-03-15 04:39:17 +0000

    Extract shortest path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -5,7 +5,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     vector<int> distances(numVertices, INF);
     vector<bool> visited(numVertices, false);
     distances[source] = 0;
-    previous[source] = UNDEFINED;
+    previous.assign(numVertices, UNDEFINED);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;    // pair<weight,vertex>
     minHeap.push({0, source});
 
@@ -27,12 +27,21 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     return distances;
 }
 
-// vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
-
 int extractVertexWithMinWeight(priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>& minHeap) {
     int vertex = minHeap.top().second;
     minHeap.pop();
     return vertex;
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
+    int numVertices = previous.size();
+    vector<int> path(numVertices, UNDEFINED);
+    int vertex = destination;
+    while (int i=numVertices-1; previous[vertex] != UNDEFINED && i>=0; --i) {
+        path[i] = vertex;
+        vertex = previous[vertex];
+    }
+    return path;
 }
 
 void print_path(const vector<int>& v, int total) {
