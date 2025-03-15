29839f53dacbd51dc61fedf8ae1276425045a15c
Audrey Lu <audreyylu04@gmail.com>
2025-03-15 01:01:25 -0700

    Run test instead of define my_assert

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -93,15 +93,23 @@ void print_word_ladder(const vector<string>& ladder) {
 }
 
 void verify_word_ladder() {
-    #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+    // #define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 
     set<string> word_list;
     load_words(word_list, "src/words.txt");
 
-    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
-    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
-    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
-    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+    run_test("cat", "dog", word_list, 4);
+    run_test("marty", "curls", word_list, 6);
+    run_test("code", "data", word_list, 6);
+    run_test("car", "cheat", word_list, 4);
 
-    #undef my_assert
+    // #undef my_assert
+}
+
+void run_test(string begin_word, string end_word, set<string> word_list, int expected_size) {
+    cout << "generate_word_ladder(\"" << begin_word << "\", \"" << end_word << "\", word_list).size() == " << expected_size << " ";
+    if (generate_word_ladder(begin_word, end_word, word_list).size() == expected_size)
+        cout << "passed" << endl;
+    else
+        cout << "failed" << endl;
 }
