27289281e5d2ce00a613e6181b3c4e245d7d5513
Audrey Lu <luay5@uci.edu>
2025-03-15 04:48:37 +0000

    Test Dijkstra's on small.txt

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,1 +1,13 @@
 #include <dijkstras.h>
+
+int main() {
+    Graph graph;
+    vector<int> previous;
+    file_to_graph("src/small.txt", graph);
+    vector<int> distances = dijkstra_shortest_path(graph, 0, previous);
+
+    int dest = 2;
+    int distance = distances[dest];
+    vector<int> path = extract_shortest_path(previous, dest);
+    print_path(path, distance);  // 0 3 1 2
+}
