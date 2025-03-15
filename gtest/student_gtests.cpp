4c9d3f086d81dd499851aa01f8b18997784fd7f2
Audrey Lu <luay5@uci.edu>
2025-03-15 05:01:24 +0000

    Test Dijkstra's produces the correct shortest path

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,15 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+TEST(DijkstrasTest, SmallGraph) {
+  Graph graph;
+  vector<int> previous;
+  file_to_graph("src/small.txt", graph);
+  vector<int> distances = dijkstra_shortest_path(graph, 0, previous);
+
+  int dest = 2;
+  int distance = distances[dest];
+  vector<int> path = extract_shortest_path(distances, previous, dest);
+  vector<int> expected = {0, 3, 1, 2};
+  EXPECT_EQ(path, expected) << "Error: path from vertex 0 to vertex 2 is incorrect";
 }
